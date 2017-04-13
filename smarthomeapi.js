356be91d9ed8e1a72f8b9dd5aff7a988f7e585d4
Peng Liu <peng@tuxera.com>
2017-04-14 01:32:07 +0300

    Initial commit with basic mock data.

---
+++ smarthomeapi.js
@@ -0,0 +1,48 @@
+var http      = require('http');
+var express   = require('express');
+
+var app       = express();
+
+// dummy input port values for our example
+var inputs = [    { pin: '11', gpio: '17', value: 1 },
+                  { pin: '12', gpio: '18', value: 0 }
+                ];
+
+// ------------------------------------------------------------------------
+// configure Express to serve index.html and any other static pages stored 
+// in the home directory
+app.use(express.static(__dirname));
+
+// Express route for incoming requests for a single input
+app.get('/inputs/:id', function (req, res) {
+  // send an object as a JSON string
+  console.log('id = ' + req.params.id);
+  res.send(inputs[req.params.id]);
+}); // apt.get()
+
+// Express route for incoming requests for a list of all inputs
+app.get('/inputs', function (req, res) {
+  // send an object as a JSON string
+  console.log('all inputs');
+  res.status(200).send(inputs);
+}); // apt.get()
+
+// Express route for any other unrecognised incoming requests
+app.get('*', function (req, res) {
+  res.status(404).send('Unrecognised API call');
+});
+
+// Express route to handle errors
+app.use(function (err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send('Oops, Something went wrong!');
+  } else {
+    next(err);
+  }
+}); // apt.use()
+
+// ------------------------------------------------------------------------
+// Start Express App Server
+//
+app.listen(3000);
+console.log('App Server is listening on port 3000');
